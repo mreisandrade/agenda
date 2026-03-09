9a17faad0d0c8b11839be7e6dbc33c42aeb4fdae
Matheus Andrade <mat.reis.andrade@live.com>
2026-03-09 19:59:21 -0400

    Configurando a área administrativa do model Contact

--- contact/admin.py
+++ contact/admin.py
@@ -11,4 +11,51 @@ from contact import models
 # abaixo. Recebe como parâmentro o model criado.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    # Lista de campos mostrados na área admin
+    # É um iterável (tupla ou lista)
+    list_display = [
+        # O id é criado automaticamente
+        'id',
+        'first_name',
+        'last_name',
+        'phone',
+    ]
+
+    # Ordenando as listas
+    # Colocar um menos (-) antes do campo, ordena de forma
+    # descrescente (sem nada, é ordem crescente)
+    ordering = [
+        '-id',
+    ]
+
+    # Adiciona filtros
+    # list_filter = [
+    #     'created_date',
+    # ]
+
+    # Campos de pesquisa
+    search_fields = [
+        'id',
+        'first_name',
+        'last_name',
+    ]
+
+    # Configura o número de contatos por página
+    list_per_page = 10
+
+    # Máximo de dados mostrados em "Mostrar Tudo"
+    list_max_show_all = 200
+
+    # Adiciona uma forma de editar os valores diretamente
+    list_editable = [
+        'first_name',
+        'last_name',
+    ]
+
+    # Torna aquele campo um link para abrir o dado
+    # O mesmo campo não pode estar no list_editable e no
+    # list_display_links ao mesmo tempo
+    list_display_links = [
+        'id',
+        'phone',
+    ]
