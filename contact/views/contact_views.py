cfede61df90b5a91dcf4ff51558adc56bf392ba7
Matheus Andrade <mat.reis.andrade@live.com>
2026-03-14 14:06:35 -0400

    Criando a tabela de dados dos contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+# Importando o model dos contatos (database)
+from contact.models import Contact
 
 
 # Create your views here.
 def index(request):
+    # Coletando os dados dos contatos
+    contacts = Contact.objects.all()
+
+    # Aquivos que são enviados para a view
+    context = { 
+        'contacts': contacts,
+    }
+
     return render(
         request,
         'contact/index.html',
+        context=context,
     )   
