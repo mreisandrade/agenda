27206d0ee0b83eb3f16a03d9dad77c29c92655fa
Matheus Andrade <mat.reis.andrade@live.com>
2026-03-22 20:27:27 -0400

    Adionando uma mensagem de sucesso na criação do usuário.

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# Usado para enviar flash messages
+from django.contrib import messages
 
 from contact.forms import RegisterForm
 
 
+''' Flash messages
+    - Enviar mensagems para a view
+    - A mensagem pode ser enviada de qualquer lugar
+    - Só vai deixar de existir quando for exibida em algum
+    template qualquer
+    - Tipo:
+        + warning: alerta
+        + sucess: sucesso
+        + info: informação
+        + error: erro 
+    - Uma mensagem precisa ser capturada em algum lugar
+'''
+
+
 def register(request):
     form = RegisterForm()
+
+    # Mensagem registrada
+    # messages.info(request, 'Um texto qualquer')
+    # messages.success(request, 'Um texto qualquer')
+    # messages.warning(request, 'Um texto qualquer')
+    # messages.error(request, 'Um texto qualquer')
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:index') 
     
     return render(
         request,
